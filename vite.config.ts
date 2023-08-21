d2da8870377c096e20b3cefa4d808cb5dd8cccc7
Emerson Demetrio <emerson@drivekyte.com>
2023-08-21 18:50:12 +0200

    [0] change dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
+    build: {
+      outDir: './docs', // This will be overrided to `dist` by qwikVite() setting
+    },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
