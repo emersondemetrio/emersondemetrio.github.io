d73d7b6561b0af04b481f4050c680667c9e7d897
Emerson Demetrio <emerson@drivekyte.com>
2023-08-25 16:06:50 +0200

    [0] add hover

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,22 +3,11 @@ import { HomeUrl } from '../../constants';
 const Footer = () => {
 	return (
 		<footer className="footer" id="footer">
-			<div className="container">
-				<div className="row align-items-center">
-					<div className="col-md-4">
-						<span className="copyright">
-							Copyright &copy;{' '}
-							<a href={HomeUrl.url}>{HomeUrl.title}</a>{' '}
-							<span>{new Date().getFullYear()}</span>
-						</span>
-					</div>
-
-					<div className="col-md-4">
-						<ul className="list-inline social-buttons"></ul>
-					</div>
-
-					<div className="col-md-4"></div>
-				</div>
+			<div className="col-md-4">
+				<span className="copyright">
+					Copyright &copy; <a href={HomeUrl.url}>{HomeUrl.title}</a>{' '}
+					<span>{new Date().getFullYear()}</span>
+				</span>
 			</div>
 		</footer>
 	);
