ac265373a8c54f2feab34dfac674914200b719f1
Emerson Demetrio <emerson@drivekyte.com>
2024-03-15 10:09:43 -0300

    [000] Add categories

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@ export type Link = {
 	title: string;
 	url: string;
 	icon?: string;
+	category: 'social' | 'professional' | 'arts';
 };
 
 export const HomeUrl = {
@@ -13,40 +14,47 @@ export const Links: Array<Link> = [
 	{
 		title: 'Instagram',
 		url: 'https://www.instagram.com/emersondemetrio/',
+		category: 'social'
 	},
 	{
 		title: 'Twitter',
 		url: 'https://www.twitter.com/emersondemetrio/',
-	},
-	{
-		title: 'GitHub',
-		url: 'https://www.github.com/emersondemetrio/',
-	},
-	{
-		title: 'SoundCloud',
-		url: 'https://soundcloud.com/emersondemetrio/tracks',
+		category: 'social'
 	},
 	{
 		title: 'LinkedIn',
 		url: 'https://www.linkedin.com/in/emersondemetrio/',
+		category: 'professional'
+	},
+	{
+		title: 'GitHub',
+		url: 'https://www.github.com/emersondemetrio/',
+		category: 'professional'
 	},
 	{
 		title: 'Gist',
 		url: 'https://gist.github.com/emersondemetrio',
+		category: 'professional'
+	},
+	{
+		title: 'CodePen',
+		url: 'https://codepen.io/emersondemetrio/',
+		category: 'professional'
 	},
 	{
 		title: 'YouTube',
 		url: 'https://www.youtube.com/@emersondemetrio/videos',
+		category: 'arts'
+	},
+	{
+		title: 'SoundCloud',
+		url: 'https://soundcloud.com/emersondemetrio/tracks',
+		category: 'arts'
 	},
 	{
 		title: 'Blog',
 		url: 'https://badcompiler.tumblr.com',
+		category: 'arts'
 	},
 ];
 
-export const OtherStuff: Array<Link> = [
-	{
-		title: 'Bad Compiler',
-		url: 'https://badcompiler.tumblr.com',
-	},
-];
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,16 +2,69 @@ import 'react';
 import { Link, Links } from '../../constants';
 import './home.css';
 
-const CustomLink = ({ title, url }: Link) => {
+const CustomLink = ({ title, url, category }: Link) => {
 	return (
 		<a
 			href={url}
 			key={title}
 			target="_blank"
-			className="grid-item text-uppercase"
+			className={`grid-item text-uppercase ${category}`}
 		>
 			<div key={title}>{title}</div>
 		</a>
+	);
+};
+
+const groupByCategory = (items: Link[]) => {
+	return items.reduce((acc, item) => {
+		const { category } = item;
+		if (!acc[category as string]) {
+			acc[category as string] = [];
+		}
+		acc[category as string].push(item);
+		return acc;
+	}, {} as Record<string, Link[]>);
+};
+
+const Header = () => {
+	return (
+		<div className="row">
+			<div
+				className="col-lg-12 text-center"
+				style={{
+					marginBottom: 20,
+				}}
+			>
+				<h3 className="section-heading text-primary black-bg">
+					Emerson Demetrio
+				</h3>
+				<p className="text-primary black-bg">
+					Software Engineer and musician.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+const CategoryGroups = ({ group, links }: { group: string; links: Link[] }) => {
+	return (
+		<div className="">
+			<div className="black-bg pl10 mt10 mb10">
+				<p className="reset group-title text-primary white-text">
+					{group}
+				</p>
+			</div>
+			<div className="grid-container">
+				{links.map(({ title, url, category }) => (
+					<CustomLink
+						key={title}
+						title={title}
+						url={url}
+						category={category}
+					/>
+				))}
+			</div>
+		</div>
 	);
 };
 
@@ -20,32 +73,16 @@ export const Home = () => {
 		<>
 			<section className="page-section" id="services">
 				<div className="container">
-					<div className="row">
-						<div
-							className="col-lg-12 text-center"
-							style={{
-								marginBottom: 20,
-							}}
-						>
-							<h3 className="section-heading text-primary black-bg">
-								Emerson Demetrio
-							</h3>
-							<p className="text-primary black-bg">
-								Software Engineer and musician.
-							</p>
-						</div>
-					</div>
-					<div className="black-bg">
-						<h3 className="white-text">Stuff</h3>
-					</div>
-					<div className="grid-container">
-						{Links.map(({ title, url }) => (
-							<CustomLink key={title} title={title} url={url} />
-						))}
-					</div>
-					{/* <div className="black-bg mt5">
-						<h3 className="white-text">Other Stuff</h3>
-					</div> */}
+					<Header />
+					{Object.entries(groupByCategory(Links)).map(
+						([group, links]) => (
+							<CategoryGroups
+								key={group}
+								group={group}
+								links={links}
+							/>
+						)
+					)}
 				</div>
 			</section>
 		</>
