d63cb604fc3150dc0bb8cafac9316af019588805
Emerson Demetrio <emerson@drivekyte.com>
2024-06-28 12:17:58 +0200

    [0000] Add code link

--- src/pages/experiments/experiments.tsx
+++ src/pages/experiments/experiments.tsx
@@ -1,55 +1,77 @@
 import "react";
 import { Page } from "@/components/page/page";
 import { Link } from "react-router-dom";
+const experiments = [
+  {
+    title: "Remove Background",
+    description: "Remove background from images 🖼️",
+    link: "/bg",
+    repo: "https://github.com/emersondemetrio/emersondemetrio.github.io/blob/main/src/pages/remove-background/remove-background.tsx",
+  },
+  {
+    title: "Canvas Game",
+    description: "Simple game using canvas 🪨✂️📜",
+    link: "/game",
+    repo: "https://github.com/emersondemetrio/emersondemetrio.github.io/blob/main/src/pages/canvas-game/canvas-game.tsx",
+  },
+];
 
 export const Experiments = () => {
   return (
-    <Page >
+    <Page>
       <h1>Experiments</h1>
       <p>Some experiments I've been working on</p>
 
-      <div className="row" style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        flexWrap: "wrap",
-        padding: 20,
-      }}>
-        {[
-          {
-            title: "Remove Background",
-            description: "Remove background from images",
-            link: "/bg",
-          },
-          {
-            title: "Canvas Game",
-            description: "Simple game using canvas",
-            link: "/game",
-          },
-        ].map((item) => (
-          <div key={item.title} style={{
-            border: "solid 1px #ddd",
-            margin: 5,
-            padding: 5,
-            borderRadius: 5,
-            width: '500px',
-            height: '300px',
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignContent: "center",
-            alignItems: "center",
-          }}>
+      <div
+        className="row"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          padding: 20,
+        }}
+      >
+        {experiments.map((item) => (
+          <div
+            key={item.title}
+            style={{
+              border: "solid 1px #ddd",
+              margin: 5,
+              padding: 5,
+              borderRadius: 5,
+              width: "500px",
+              height: "300px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignContent: "center",
+              alignItems: "center",
+            }}
+          >
             <h2>{item.title}</h2>
             <p>{item.description}</p>
-            <div>
-              <Link className="btn btn-light" to={item.link}>Go to {item.title}</Link>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                padding: 10,
+                alignItems: "center",
+              }}
+            >
+              <Link className="btn btn-light" to={item.link}>
+                See live: {item.title}
+              </Link>
+              <br />
+              <Link className="btn btn-secondary" to={item.repo}>
+                Show me the code
+              </Link>
             </div>
           </div>
         ))}
       </div>
-
     </Page>
   );
 };
