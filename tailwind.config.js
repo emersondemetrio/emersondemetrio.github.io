add08a54fd9e3f19cc73427c1e49dbc615bff1ba
Emerson Demetrio <emerson@drivekyte.com>
2024-07-22 14:22:02 +0200

    [0000] Force dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,44 @@
-import daisyui from "daisyui";
+import daisyui from 'daisyui';
 
 export default {
-  content: ["./src/**/*.{html,js,tsx}"],
+  content: ['./src/**/*.{html,js,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
+  darkMode: 'selector',
   daisyui: {
     themes: [
       {
-        "emerson.run": {
-          primary: "#0068ff",
-          "primary-content": "#ffffff",
+        'emerson.run': {
+          primary: '#0068ff',
+          'primary-content': '#ffffff',
 
-          secondary: "#6f9600",
-          "secondary-content": "#ffffff",
+          secondary: '#6f9600',
+          'secondary-content': '#ffffff',
 
-          accent: "#007200",
-          "accent-content": "#ffffff",
+          accent: '#007200',
+          'accent-content': '#ffffff',
 
-          neutral: "#003335",
-          "neutral-content": "#ffffff",
+          neutral: '#003335',
+          'neutral-content': '#ffffff',
 
-          "base-100": "#000000",
-          "base-200": "#1a1a1a",
-          "base-300": "#333333",
-          "base-content": "#ffffff",
+          'base-100': '#000000',
+          'base-200': '#1a1a1a',
+          'base-300': '#333333',
+          'base-content': '#ffffff',
 
-          info: "#00e3ff",
-          "info-content": "#000000",
+          info: '#00e3ff',
+          'info-content': '#000000',
 
-          success: "#1cb024",
-          "success-content": "#000000",
+          success: '#1cb024',
+          'success-content': '#000000',
 
-          warning: "#fab000",
-          "warning-content": "#000000",
+          warning: '#fab000',
+          'warning-content': '#000000',
 
-          error: "#ff4c57",
-          "error-content": "#000000",
+          error: '#ff4c57',
+          'error-content': '#000000',
         },
       },
     ],
