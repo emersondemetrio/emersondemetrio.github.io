f6cbcce4ede311d46da548950109b8598a84c4cb
Emerson Demetrio <emer.demetrio@gmail.com>
2024-09-16 21:34:57 +0200

    [0000] Fix css

--- src/pages/camera/camera.tsx
+++ src/pages/camera/camera.tsx
@@ -68,7 +68,7 @@ export const Camera = () => {
           )}
           <button
             onClick={download}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 flex items-center gap-2"
+            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 flex items-center gap-2"
           >
             <span>📸</span>
             Capture
